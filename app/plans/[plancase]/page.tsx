1964afa54d4c9b20f218cbff3ce47d8996dd7716
Parv <kulparv@gmail.com>
2025-01-28 22:44:37 +0530

    build

--- app/plans/[plancase]/page.tsx
+++ app/plans/[plancase]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { planData, PlanInfo } from "../../data/planData";
 import { GoogleGenerativeAI } from "@google/generative-ai";
